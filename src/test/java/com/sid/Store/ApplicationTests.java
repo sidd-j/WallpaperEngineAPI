12189a4eb8d1292d33252bc43ddb760e0f4ee2fc
sidd-j <53267391+sidd-j@users.noreply.github.com>
2026-04-25 02:59:15 +0530

    CICD FIXES4

--- src/test/java/com/sid/Store/ApplicationTests.java
+++ src/test/java/com/sid/Store/ApplicationTests.java
@@ -2,11 +2,12 @@ package com.sid.Store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 }
